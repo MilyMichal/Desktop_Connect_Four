5cac90824a72bdd834f09b3bb9c16f4a521a4567
Azrael <azrael.Taleri@gmail.com>
2023-06-14 14:55:54 +0200

    Stage 2/4: added window for confirm closing of application - class CheckOnExit, ClosingWindow, deleted button labels
    TO DO:
    implement method for changing button labels to either X or O depends on players turn

--- ConnectFour.java
+++ ConnectFour.java
@@ -2,16 +2,22 @@ package four;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class ConnectFour extends JFrame {
+
     public ConnectFour() {
         setTitle("Connect Four");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         setSize(450, 450);
         setResizable(false);
         setLayout(new BorderLayout());
         setLocationRelativeTo(null);
         initButtons();
+        addWindowListener(new CheckOnExit());
         setVisible(true);
     }
 
@@ -22,7 +28,7 @@ public class ConnectFour extends JFrame {
         buttonContainer.setLayout(new GridLayout(6, 7));
         for (int i = 6; i > 0; i--) {
             for (char j = 'A'; j < 'H'; j++) {
-                JButton button = new JButton(String.format("%s%d", j, i).toUpperCase());
+                JButton button = new JButton(" ");
                 button.setFocusPainted(false);
                 button.setName(String.format("Button%s%d", j, i));
                 buttonContainer.add(button).setFont(font);
@@ -30,5 +36,46 @@ public class ConnectFour extends JFrame {
         }
         setVisible(true);
         add(buttonContainer);
+    }
+}
+//confirmation window for closing application
+class CheckOnExit extends WindowAdapter {
+    public void windowClosing (WindowEvent e) {
+        ClosingWindow checker = new ClosingWindow();
+        checker.setVisible(true);
+    }
+
+}
+class ClosingWindow extends JFrame implements ActionListener {
+    public ClosingWindow() {
+        setSize(250, 100);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+        JLabel confirmLabel = new JLabel("Are you sure you want to quit?", SwingConstants.CENTER);
+        add(confirmLabel,BorderLayout.CENTER);
+
+        JPanel buttonPannel = new JPanel();
+        buttonPannel.setLayout(new FlowLayout());
+
+        JButton confirmButton = new JButton("Yes");
+        confirmButton.addActionListener(this);
+        buttonPannel.add(confirmButton);
+
+        JButton cancelButton = new JButton("No");
+        cancelButton.addActionListener(this);
+        buttonPannel.add(cancelButton);
+
+        add(buttonPannel,BorderLayout.SOUTH);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String action = e.getActionCommand();
+
+        if (action.equals("Yes")) {
+            System.exit(0);
+        } else if (action.equals("No")) {
+            dispose();
+        }
     }
 }
